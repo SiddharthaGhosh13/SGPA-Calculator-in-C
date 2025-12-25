569beef188e80da46261f8bbd069ee8e4fff6eed
SiddharthaGhosh13 <itzsiddharthaghosh13@gmail.com>
2025-12-25 09:57:03 +0530

    Update Calculator Program

--- SGPA_Calculator.C
+++ SGPA_Calculator.C
@@ -11,23 +11,23 @@ int main(){
         scanf("%f",&Marks);
         printf("(Subject%d Credit): ",i);
         scanf("%f",&Credit);
-        if(Marks>=90)
+        if(Marks>=89.5)
             Grade=10;
-        else if(Marks>=80)
+        else if(Marks>=79.5)
             Grade=9;
-        else if(Marks>=70)
+        else if(Marks>=69.5)
             Grade=8;
-        else if(Marks>=60)
+        else if(Marks>=59.5)
             Grade=7;
-        else if(Marks>=50)
+        else if(Marks>=49.5)
             Grade=6;
-        else if(Marks>=40)
+        else if(Marks>=39.5)
             Grade=5;
-        else if(Marks>=30)
+        else if(Marks>=29.5)
             Grade=4;
-        else if(Marks>=20)
+        else if(Marks>=19.5)
             Grade=3;
-        else if(Marks>=10)
+        else if(Marks>=9.5)
             Grade=2;
         else if(Marks<10)
             Grade=1;
